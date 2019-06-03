6d95f80405d1592caab98c0b19edbf2c524f0127
Jamil Hasan <Hasanfj@outlook.com>
2019-06-03 17:19:01 -0400

    ArrayCollections

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       string monday = "Monday";
+           string [] daysoftheWeek= {"Monday", "Tuesday", "Wednesday"
+
+ };
+
+     //       foreach (var item in daysoftheWeek)
+	//{
+   //             Console.WriteLine(item);
+	//}
+        Console.WriteLine("Write the value");
+        int iDay = int.Parse(Console.ReadLine());
+            string choosenDay = daysoftheWeek[iDay-1];
+            Console.WriteLine($"That day is  {choosenDay}");
+
+        }
+        }
+    }
