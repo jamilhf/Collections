f914c59acbbab35a9043f9534ed4d37878cb8c5f
Jamil Hasan <Hasanfj@outlook.com>
2019-06-04 09:13:11 -0400

    Country_csvReader

---
+++ CvsReader.cs
@@ -0,0 +1,24 @@
+namespace Collections
+{
+
+
+    public class CvsReader
+    {
+
+
+        private string CvsFilePath;
+
+        public CsvReader(string FilePath)
+        {
+            this.CvsFilePath = FilePath;
+
+        }
+
+        public Country[] ReadFirstNCountries(int nCountries)
+        {
+            return null;
+
+        }
+    }
+
+}
